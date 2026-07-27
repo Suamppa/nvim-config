213d9a1e6d360825fcbc35c89febb4362336c116
Samppa Alatalo <18483942+Suamppa@users.noreply.github.com>
2026-07-27 22:47:00 +0300

    Delete lua/custom and lua/kickstart
    Moved the remaining plugin configs to lua/plugins and enabled some.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -14,7 +14,7 @@
 
 -- Load order matters
 require 'plugins.guess-indent'
-require 'plugins.gitsigns' -- TODO: Expand gitsigns config with keymaps
+require 'plugins.gitsigns'
 require 'plugins.which-key'
 require 'plugins.colorscheme'
 require 'plugins.todo-comments'
@@ -27,26 +27,10 @@ require 'plugins.treesitter'
 require 'plugins.harpoon'
 require 'plugins.copilot'
 require 'plugins.colorizer'
-
--- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
--- init.lua. If you want these files, they are in the repository, so you can just download them and
--- place them in the correct locations.
-
--- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
---
---  Here are some example plugins that I've included in the Kickstart repository.
---  Uncomment any of the lines below to enable them (you will need to restart nvim).
---
--- require 'kickstart.plugins.debug'
-require 'kickstart.plugins.indent_line'
--- require 'kickstart.plugins.lint'
-require 'kickstart.plugins.autopairs'
--- require 'kickstart.plugins.neo-tree'
--- require 'kickstart.plugins.gitsigns' -- adds gitsigns recommended keymaps
-
--- NOTE: You can add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
---
---  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
--- require 'custom.plugins'
+require 'plugins.debug'
+require 'plugins.indent_line'
+-- require 'plugins.lint'
+require 'plugins.autopairs'
+-- require 'plugins.neo-tree'
 
 -- vim: ts=2 sts=2 sw=2 et
